3c8da9e585d67eea1e28ea76c491052a8256a8ea
aszx0413 <jordanmicle@gmail.com>
2016-12-14 11:07:06 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# PHP-SynFTP
+
+实现本地项目修改与FTP项目同步
+
+## 使用方法
+
+#### 建立项目配置文件
+
+在 `~/projects` 目录下建立项目所属的配置文件，文件名格式：`pm.php`，其中 `pm` 表示项目标识，该标识将用于运行时的参数。
+
+`pm.php` 文件代码格式如下：
+
+```
+<?php
+// 测试
+// Updated: 2016-07-10 23:43:34
+$cfg = array(
+	'root_dir' => '/Users/michael/www1/test/demo',
+	'ftp' => array(
+		'host' 		=> '10.11.12.13',
+		'username' 	=> 'www',
+		'password' 	=> 'pwd',
+		'root_dir' 	=> '/test/demo'
+	),
+	'ignored' => array(
+		'.DS_Store',
+		'.gitignore',
+		'.project',
+		'.buildpath',
+
+		'/.git',
+		'/.settings',
+	),
+);
+return $cfg;
+
+```
+其中，第一行为项目描述，第二行表示项目更新时间基准，格式应严格按照
+
+```// Updated: YYYY-MM-DD HH:ii:ss```
+
+填写，**每次运行更新后更新的时间会记录至此**。
+
+配置项 `$cfg` 的各属性分别表示：
+
+| 属性 | 描述 |
+| ------------- | ------------- |
+| root_dir | 项目根目录，绝对路径，不以“/”结束 |
+| ftp | ftp连接配置信息，其中 `root_dir` 表示FTP上项目的根目录，需要与本地目录root_dir对应 |
+| ignored | 项目忽略文件 |
+
+至此，配置完毕。
+
+#### 运行同步
+
+ - 命令行模式
+
+`$ index.php pm`
---
+++ index.php
@@ -0,0 +1,126 @@
+<?php
+date_default_timezone_set("Etc/GMT-8");
+// error_reporting(E_ALL ^ E_WARNING ^ E_NOTICE);
+error_reporting(E_ALL);
+
+if ($argc != 2) {
+	die("Usage: php index.php <PROJECT_NAME>". "\n");
+} elseif (!file_exists(dirname(__FILE__). "/projects/". $argv[1]. ".php")) {
+	die("项目 ". $argv[1]. " 不存在！\n");
+}
+$project = include_once "projects/". $argv[1]. ".php";
+$cfgFileContent = file_get_contents(dirname(__FILE__). "/projects/". $argv[1]. ".php");
+$tmp = preg_match('/\/\/\s{1}Updated:\s{1}[0-9\-:\s]{19}/', $cfgFileContent, $result);
+if (!$tmp) {
+	die("项目 ". $argv[1]. " 更新时间未设置！\n");
+}
+$lastUpdatedTs = str_ireplace("// Updated: ", "", $result[0]);
+
+$newCfgFileContent = str_ireplace($lastUpdatedTs, date('Y-m-d H:i:s'), $cfgFileContent);
+
+//
+// 需要忽略的文件
+// 如果不带“/”则表示根目录下的所有该文件均被忽略
+$ignored = $project['ignored'];
+
+define('ROOT_DIR', $project['root_dir']);
+define('FTP_ROOT_DIR', $project['ftp']['root_dir']);
+
+$lastUpdatedTs = strtotime($lastUpdatedTs); // 项目上次提交时间戳
+
+$ftpFiles = array(); //需要FTP更新的文件
+function tree($directory) {
+	global $ftpFiles, $lastUpdatedTs, $ignored;
+	$files = array(
+		'type' => 'd',
+		'name' => basename($directory),
+		'ls' => array()
+	);
+	$mydir = dir($directory);
+	while($file = $mydir->read()) {
+		if ($file != '.' && $file != '..') {
+			if(is_dir("$directory/$file")) {
+				if (!in_array("/$file", $ignored) && !in_array(str_replace(ROOT_DIR, '', $directory). "/$file", $ignored)) {
+					$files['ls'][] = tree("$directory/$file");
+				}
+			}
+			else {
+				if (!in_array($file, $ignored) && !in_array(str_replace(ROOT_DIR, '', $directory). "/$file", $ignored)) {
+					$ft = filemtime("$directory/$file");
+					if ($ft < filectime("$directory/$file")) {
+						$ft = filectime("$directory/$file");
+					}
+					$f = array('path' => str_replace(ROOT_DIR, '', $directory), 'name' => $file, 'mtime' => $ft);
+					if ($ft > $lastUpdatedTs) {
+						$ftpFiles[] = $f;
+					}
+					$files['ls'][] = $f;
+				}
+			}
+		}
+	}
+	$mydir->close();
+	return $files;
+}
+
+$files = tree(ROOT_DIR); //获取本地项目所有文件
+
+$ftp = ftp_connect($project['ftp']['host']);
+// $ftp = ftp_ssl_connect($project['ftp']['host']);
+if (!$ftp) {
+	die('ftp cannot connect'. "\n");
+}
+
+$ftpLogin = ftp_login($ftp, $project['ftp']['username'], $project['ftp']['password']);
+if (!$ftpLogin) {
+	die('ftp cannot login'. "\n");
+}
+
+ftp_pasv($ftp, true);
+
+$cnt = 0;
+$success = 0;
+
+$lastDir = '';
+foreach ($ftpFiles as $v) {
+	echo ROOT_DIR. $v['path']. '/'. $v['name']. "\n";
+	if (FTP_ROOT_DIR. $v['path'] != $lastDir) {
+		for ($i= 0; $i< 3; $i++) {
+			if (@ftp_chdir($ftp, FTP_ROOT_DIR. $v['path'])) {
+				$inDir = true;
+				break;
+			} else {
+				echo "- Can't ftp_chdir to ". FTP_ROOT_DIR. $v['path']. "\n";
+			}
+		}
+		if (!$inDir) {
+			if (!ftp_mkdir($ftp, FTP_ROOT_DIR. $v['path'])) {
+				die("- ftp_mkdir failed: ". FTP_ROOT_DIR. $v['path']. "\n");
+			}
+			@ftp_chdir($ftp, FTP_ROOT_DIR. $v['path']);
+		}
+		$inDir = false;
+		$lastDir = FTP_ROOT_DIR. $v['path'];
+	}
+
+	// 传输模式有 FTP_ASCII/FTP_BINARY
+	$ret = ftp_put($ftp, $v['name'], ROOT_DIR. $v['path']. '/'. $v['name'], FTP_BINARY);
+
+	if ($ret) {
+		$success++;
+	} else {
+		var_dump($ret);
+		echo "- Can't ftp_put ". FTP_ROOT_DIR. $v['path']. "/". $v['name']. "\n";
+	}
+	$cnt++;
+}
+
+ftp_close($ftp);
+
+echo "- Total: ". $cnt. ", Success: {$success}\n";
+
+if ($cnt == $success) {
+	file_put_contents(dirname(__FILE__). "/projects/". $argv[1]. ".php", $newCfgFileContent);
+}
+
+die;
