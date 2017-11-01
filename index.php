6737272571d2b7c7ffd504767446a2eff628adf1
Mike <>
2017-11-01 16:06:09 +0800

    add test mode

--- index.php
+++ index.php
@@ -3,20 +3,33 @@ date_default_timezone_set("Etc/GMT-8");
 // error_reporting(E_ALL ^ E_WARNING ^ E_NOTICE);
 error_reporting(E_ALL);
 
-if ($argc != 2) {
-	die("Usage: php index.php <PROJECT_NAME>". "\n");
-} elseif (!file_exists(dirname(__FILE__). "/projects/". $argv[1]. ".php")) {
-	die("项目 ". $argv[1]. " 不存在！\n");
+if ($argc < 2) {
+    die("Usage: php index.php <PROJECT_NAME>" . "\n");
+} elseif (!file_exists(dirname(__FILE__) . "/projects/" . $argv[1] . ".php")) {
+    die("项目 " . $argv[1] . " 不存在！\n");
 }
-$project = include_once "projects/". $argv[1]. ".php";
-$cfgFileContent = file_get_contents(dirname(__FILE__). "/projects/". $argv[1]. ".php");
-$tmp = preg_match('/\/\/\s{1}Updated:\s{1}[0-9\-:\s]{19}/', $cfgFileContent, $result);
+$project        = include_once "projects/" . $argv[1] . ".php";
+$cfgFileContent = file_get_contents(dirname(__FILE__) . "/projects/" . $argv[1] . ".php");
+$tmp            = preg_match('/\/\/\s{1}Updated:\s{1}[0-9\-:\s]{19}/', $cfgFileContent, $result);
 if (!$tmp) {
-	die("项目 ". $argv[1]. " 更新时间未设置！\n");
+    die("项目 " . $argv[1] . " 更新时间未设置！\n");
 }
 $lastUpdatedTs = str_ireplace("// Updated: ", "", $result[0]);
 
 $newCfgFileContent = str_ireplace($lastUpdatedTs, date('Y-m-d H:i:s'), $cfgFileContent);
+
+$doFtp = true;
+
+/*
+ * 如果带参数-t，
+ * 则表示只列出当前需要更新的文件列表，
+ * 不进行FTP上传
+ */
+if (isset($argv[2])) {
+    if ($argv[2] == '-t') {
+        $doFtp = false;
+    }
+}
 
 //
 // 需要忽略的文件
@@ -29,98 +42,106 @@ define('FTP_ROOT_DIR', $project['ftp']['root_dir']);
 $lastUpdatedTs = strtotime($lastUpdatedTs); // 项目上次提交时间戳
 
 $ftpFiles = array(); //需要FTP更新的文件
-function tree($directory) {
-	global $ftpFiles, $lastUpdatedTs, $ignored;
-	$files = array(
-		'type' => 'd',
-		'name' => basename($directory),
-		'ls' => array()
-	);
-	$mydir = dir($directory);
-	while($file = $mydir->read()) {
-		if ($file != '.' && $file != '..') {
-			if(is_dir("$directory/$file")) {
-				if (!in_array("/$file", $ignored) && !in_array(str_replace(ROOT_DIR, '', $directory). "/$file", $ignored)) {
-					$files['ls'][] = tree("$directory/$file");
-				}
-			}
-			else {
-				if (!in_array($file, $ignored) && !in_array(str_replace(ROOT_DIR, '', $directory). "/$file", $ignored)) {
-					$ft = filemtime("$directory/$file");
-					if ($ft < filectime("$directory/$file")) {
-						$ft = filectime("$directory/$file");
-					}
-					$f = array('path' => str_replace(ROOT_DIR, '', $directory), 'name' => $file, 'mtime' => $ft);
-					if ($ft > $lastUpdatedTs) {
-						$ftpFiles[] = $f;
-					}
-					$files['ls'][] = $f;
-				}
-			}
-		}
-	}
-	$mydir->close();
-	return $files;
+
+function tree($directory)
+{
+    global $ftpFiles, $lastUpdatedTs, $ignored;
+    $files = array(
+        'type' => 'd',
+        'name' => basename($directory),
+        'ls'   => array(),
+    );
+    $mydir = dir($directory);
+    while ($file = $mydir->read()) {
+        if ($file != '.' && $file != '..') {
+            if (is_dir("$directory/$file")) {
+                if (!in_array("/$file", $ignored) && !in_array(str_replace(ROOT_DIR, '', $directory) . "/$file", $ignored)) {
+                    $files['ls'][] = tree("$directory/$file");
+                }
+            } else {
+                if (!in_array($file, $ignored) && !in_array(str_replace(ROOT_DIR, '', $directory) . "/$file", $ignored)) {
+                    $ft = filemtime("$directory/$file");
+                    if ($ft < filectime("$directory/$file")) {
+                        $ft = filectime("$directory/$file");
+                    }
+                    $f = array('path' => str_replace(ROOT_DIR, '', $directory), 'name' => $file, 'mtime' => $ft);
+                    if ($ft > $lastUpdatedTs) {
+                        $ftpFiles[] = $f;
+                    }
+                    $files['ls'][] = $f;
+                }
+            }
+        }
+    }
+    $mydir->close();
+    return $files;
 }
 
 $files = tree(ROOT_DIR); //获取本地项目所有文件
 
-$ftp = ftp_connect($project['ftp']['host']);
+if ($doFtp) {
+    $ftp = ftp_connect($project['ftp']['host']);
 // $ftp = ftp_ssl_connect($project['ftp']['host']);
-if (!$ftp) {
-	die('ftp cannot connect'. "\n");
-}
+    if (!$ftp) {
+        die('ftp cannot connect' . "\n");
+    }
 
-$ftpLogin = ftp_login($ftp, $project['ftp']['username'], $project['ftp']['password']);
-if (!$ftpLogin) {
-	die('ftp cannot login'. "\n");
-}
+    $ftpLogin = ftp_login($ftp, $project['ftp']['username'], $project['ftp']['password']);
+    if (!$ftpLogin) {
+        die('ftp cannot login' . "\n");
+    }
 
 // ftp_pasv($ftp, true);
+}
 
-$cnt = 0;
+$cnt     = 0;
 $success = 0;
 
 $lastDir = '';
 foreach ($ftpFiles as $v) {
-	echo ROOT_DIR. $v['path']. '/'. $v['name']. "\n";
-	if (FTP_ROOT_DIR. $v['path'] != $lastDir) {
-		for ($i= 0; $i< 3; $i++) {
-			if (@ftp_chdir($ftp, FTP_ROOT_DIR. $v['path'])) {
-				$inDir = true;
-				break;
-			} else {
-				echo "- Can't ftp_chdir to ". FTP_ROOT_DIR. $v['path']. "\n";
-			}
-		}
-		if (!$inDir) {
-			if (!ftp_mkdir($ftp, FTP_ROOT_DIR. $v['path'])) {
-				die("- ftp_mkdir failed: ". FTP_ROOT_DIR. $v['path']. "\n");
-			}
-			@ftp_chdir($ftp, FTP_ROOT_DIR. $v['path']);
-		}
-		$inDir = false;
-		$lastDir = FTP_ROOT_DIR. $v['path'];
-	}
+    echo ROOT_DIR . $v['path'] . '/' . $v['name'] . "\n";
 
-	// 传输模式有 FTP_ASCII/FTP_BINARY
-	$ret = ftp_put($ftp, $v['name'], ROOT_DIR. $v['path']. '/'. $v['name'], FTP_BINARY);
+    if ($doFtp) {
+        if (FTP_ROOT_DIR . $v['path'] != $lastDir) {
+            for ($i = 0; $i < 3; $i++) {
+                if (@ftp_chdir($ftp, FTP_ROOT_DIR . $v['path'])) {
+                    $inDir = true;
+                    break;
+                } else {
+                    echo "- Can't ftp_chdir to " . FTP_ROOT_DIR . $v['path'] . "\n";
+                }
+            }
+            if (!$inDir) {
+                if (!ftp_mkdir($ftp, FTP_ROOT_DIR . $v['path'])) {
+                    die("- ftp_mkdir failed: " . FTP_ROOT_DIR . $v['path'] . "\n");
+                }
+                @ftp_chdir($ftp, FTP_ROOT_DIR . $v['path']);
+            }
+            $inDir   = false;
+            $lastDir = FTP_ROOT_DIR . $v['path'];
+        }
 
-	if ($ret) {
-		$success++;
-	} else {
-		var_dump($ret);
-		echo "- Can't ftp_put ". FTP_ROOT_DIR. $v['path']. "/". $v['name']. "\n";
-	}
-	$cnt++;
+        // 传输模式有 FTP_ASCII/FTP_BINARY
+        $ret = ftp_put($ftp, $v['name'], ROOT_DIR . $v['path'] . '/' . $v['name'], FTP_BINARY);
+
+        if ($ret) {
+            $success++;
+        } else {
+            var_dump($ret);
+            echo "- Can't ftp_put " . FTP_ROOT_DIR . $v['path'] . "/" . $v['name'] . "\n";
+        }
+        $cnt++;
+    }
 }
 
-ftp_close($ftp);
+if ($doFtp) {
+    ftp_close($ftp);
 
-echo "- Total: ". $cnt. ", Success: {$success}\n";
+    echo "- Total: " . $cnt . ", Success: {$success}\n";
 
-if ($cnt == $success) {
-	file_put_contents(dirname(__FILE__). "/projects/". $argv[1]. ".php", $newCfgFileContent);
+    if ($cnt == $success) {
+        file_put_contents(dirname(__FILE__) . "/projects/" . $argv[1] . ".php", $newCfgFileContent);
+    }
 }
 
 die;
